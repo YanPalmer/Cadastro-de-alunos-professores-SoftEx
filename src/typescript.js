58f578d64ea19ff42b5fb80c13fd91548e98032d
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-08-24 19:20:04 -0300

    Código removido

--- src/typescript.js
+++ src/typescript.js
@@ -3,24 +3,41 @@ let botao = document.getElementById("botao");
 
 botao.addEventListener("click", criarAluno);
 
-function criarAluno(evento) {
-  criarDiv();
-
-  // let listaAluno = document.createElement("h3");
-  // listaAluno.innerHTML = aluno;
+function criarAluno() {
   
-  // document.getElementById("alunos").appendChild(listaAluno);
+
+
+  // let div = criarDiv();
 }
 
-function criarDiv() {
-  let div = document.createElement("div");
-  div.id = "aluno"
-  div.innerHTML = getAluno();
 
-  document.getElementById("lista").appendChild(div);
-}
 
-function getAluno() {
-  const aluno = entrada.value
-  return aluno
-}
+
+
+
+
+
+
+
+
+
+
+
+
+// function criarDiv() {
+//   let li = document.createElement("li");
+//   li.id = "aluno"
+
+//   // Atributos:
+//   li.style.position = "relative";
+//   li.style.border = "1px white solid";
+//   li.style.padding = "10px 20px";
+//   li.style.width = "100%";
+//   li.style.height = "60px";
+//   li.style.display = "flex";
+//   li.style.alignItems = "center";
+//   li.style.marginTop = "2px";
+
+//   document.getElementById("lista").appendChild(li);
+  
+// }
